6140e3867003d1d8d51704f448ea1db0ee5f9c87
romeo mureithi <romeomureithi@gmail.com>
2022-04-25 20:11:01 +0300

    Added Linear Search

---
+++ Searching/LinearSearch.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * @param Arr The array containing the elements
+ * @param number Number of elements in the array
+ * @param data The number searched in the array
+ * @return int 
+ */
+int LinearSearch(int Arr[], int number, int data) {
+    for(int i = 0; i < number; i++)
+        if(Arr[i] == data)
+            return i;
+        return -1;
+}
+
+// driver code
+int main(void) {
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int data = 10;
+    int number = sizeof(arr) / sizeof(arr[0]);
+
+    int result = LinearSearch(arr, number, data);
+
+    (result == -1)
+        ? cout << "Element not in the array"
+        : cout << "Element found at index " << result;
+
+    return 0;
+}
